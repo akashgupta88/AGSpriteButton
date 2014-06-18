513e2ceca1913f3096c0c8622e5131d611fe7832
akashgupta88 <user@Akashs-Mac-mini.local>
2014-06-18 17:11:38 +0530

    first commit

---
+++ AGSpriteButton/AGSpriteButton.h
@@ -0,0 +1,13 @@
+//
+//  AGSpriteButton.h
+//  AGSpriteButton
+//
+//  Created by Akash Gupta on 18/06/14.
+//  Copyright (c) 2014 Akash Gupta. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface AGSpriteButton : SKSpriteNode
+
+@end
---
+++ AGSpriteButton/AGSpriteButton.m
@@ -0,0 +1,13 @@
+//
+//  AGSpriteButton.m
+//  AGSpriteButton
+//
+//  Created by Akash Gupta on 18/06/14.
+//  Copyright (c) 2014 Akash Gupta. All rights reserved.
+//
+
+#import "AGSpriteButton.h"
+
+@implementation AGSpriteButton
+
+@end
